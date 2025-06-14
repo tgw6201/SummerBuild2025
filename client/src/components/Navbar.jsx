fdc91bcab5e422adb8ae3041939a2173c3b74ecc
Sia Yi Zhen <yizhensia@gmail.com>
2025-06-15 06:09:25 +0800

    Improved the chatbot and created a navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">Chatbot</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/profile">Profile</Link>
+      <Link to="/settings">Settings</Link>
+      {/* Add more links as needed */}
+    </nav>
+  );
+}
