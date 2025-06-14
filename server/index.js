892854cc367c657be6586f0acd328bafa2246f85
tgw6201 <guangweitan99@hotmail.com>
2025-06-14 11:49:06 +0800

    Added routes for all tables (Testing WIP)

--- server/index.js
+++ server/index.js
@@ -227,6 +227,374 @@ app.put('/dietary-preferences', async (req, res) => {
     }
 });
 
+//Connsumed meals
+
+// Create consumed meal
+app.post('/consumed-meals', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+    const { mid, day, date } = req.body;
+    try {
+        const result = await pool.query(
+            "INSERT INTO consumed_meals (mid, userid, day, date) VALUES ($1, $2, $3, $4) RETURNING *",
+            [mid, user.rows[0].userid, day, date]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// read consumed meals
+app.get('/consumed-meals', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    try {
+        const result = await pool.query("SELECT * FROM consumed_meals WHERE userid = $1", [user.rows[0].userid]);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+// Delete consumed meal
+app.delete('/consumed-meals/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    try {
+        const result = await pool.query("DELETE FROM consumed_meals WHERE cmid = $1 AND userid = $2 RETURNING *", [id, user.rows[0].userid]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Consumed meal not found" });
+        }
+        res.json({ message: "Consumed meal deleted successfully" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//Update consumed meal
+app.put('/consumed-meals/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    const { mid, day, date } = req.body;
+    try {
+        const result = await pool.query(
+            "UPDATE consumed_meals SET mid = $1, day = $2, date = $3 WHERE id = $4 AND userid = $5 RETURNING *",
+            [mid, day, date, id, user.rows[0].userid]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Consumed meal not found" });
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// Create saved user meals
+app.post('/saved-meals', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { mid, mname, mtype, calories } = req.body;
+    try {
+        const result = await pool.query(
+            "INSERT INTO saved_user_meals (mid, userid, mname, mtype, calories) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+            [mid,user.rows[0].userid, mname, mtype, calories]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// Read saved user meals
+app.get('/saved-meals', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    try {
+        const result = await pool.query("SELECT * FROM saved_user_meals WHERE userid = $1 ORDER BY mid ", [user.rows[0].userid]);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// Delete saved user meal
+app.delete('/saved-meals/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    try {
+        const result = await pool.query("DELETE FROM saved_user_meals WHERE mid = $1 AND userid = $2 RETURNING *", [id, user.rows[0].userid]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Saved meal not found" });
+        }
+        res.json({ message: "Saved meal deleted successfully" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+// Update saved user meal
+app.put('/saved-meals/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    const { mname, mtype, calories } = req.body;
+    try {
+        const result = await pool.query(
+            "UPDATE saved_user_meals SET mname = $1, mtype = $2, calories = $3 WHERE mid = $4 AND userid = $5 RETURNING *",
+            [mname, mtype, calories, id, user.rows[0].userid]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Saved meal not found" });
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//Create Chatbot History
+app.post('/chatbot-history', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { query, response } = req.body;
+    try {
+        const result = await pool.query(
+            "INSERT INTO chatbot_history_table (userid, date, user_question, ai_response) VALUES ($1, CURRENT_DATE, $2, $3) RETURNING *",
+            [user.rows[0].userid, query, response]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//Get Chatbot History
+app.get('/chatbot-history', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    try {
+        const result = await pool.query("SELECT * FROM chatbot_history_table WHERE userid = $1 ORDER BY cid DESC", [user.rows[0].userid]);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+//Delete Chatbot History
+app.delete('/chatbot-history/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    try {
+        const result = await pool.query("DELETE FROM chatbot_history_table WHERE id = $1 AND userid = $2 RETURNING *", [id, user.rows[0].userid]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Chatbot history not found" });
+        }
+        res.json({ message: "Chatbot history deleted successfully" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//Get user recipie table
+app.get('/user-recipies', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    try {
+        const result = await pool.query("SELECT * FROM user_recipie_table WHERE userid = $1", [user.rows[0].userid]);
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+//Create user recipie
+app.post('/user-recipies', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { mname, ingredients, instructions } = req.body;
+    try {
+        const result = await pool.query(
+            "INSERT INTO user_recipie_table (userid, mname, recipe_instruction, recipe_ingredients) VALUES ($1, $2, $3, $4) RETURNING *",
+            [user.rows[0].userid, mname, instructions, ingredients]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//Update user recipie
+app.put('/user-recipies/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    const { mname, ingredients, instructions } = req.body;
+    try {
+        const result = await pool.query(
+            "UPDATE user_recipie_table SET mname = $1, recipe_ingredients = $2, recipe_instruction = $3 WHERE mid = $4 AND userid = $5 RETURNING *",
+            [mname, ingredients, instructions, id, user.rows[0].userid]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User recipie not found" });
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+//Delete user recipie
+app.delete('/user-recipies/:id', async (req, res) => {
+    const sessionid = req.cookies.sessionid;
+    if (!sessionid) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    // Get useremail from sessionid
+    const user = await pool.query("SELECT userid FROM user_login_table WHERE sessionid = $1", [sessionid]);
+    if (user.rows.length === 0) {
+        return res.status(404).json({ message: "invalid session id" });
+    }
+
+    const { id } = req.params;
+    try {
+        const result = await pool.query("DELETE FROM user_recipie_table WHERE mid = $1 AND userid = $2 RETURNING *", [id, user.rows[0].userid]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User recipie not found" });
+        }
+        res.json({ message: "User recipie deleted successfully" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
 app.listen(5000,()=>{
     console.log("Server is running on port 5000")
 });
