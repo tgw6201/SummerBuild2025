3b26b4a855187890112535ffdc971ce619b2f66b
erinarin034 <KHOO0205@e.ntu.edu.sg>
2025-06-16 23:08:36 +0800

    Fixed nav bar from showing and fixed the nav bar navigation

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,26 +2,12 @@ import React, { useEffect } from 'react';
 import '../css/Dashboard.css';
 
 export default function Dashboard() {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  
-=======
-=======
->>>>>>> Stashed changes
 
->>>>>>> Stashed changes
   return (
     <div className="dashboard-container">
       <header>
         <h1>Homepage – Dashboard</h1>
       </header>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     </div>
   );
 
